1b5b9ab6b6a53b89647ff0d906813d93f2de60bd
Kieu_Trang <kieutrang22032000@gmail.com>
2022-06-09 00:13:16 +0700

    update movieList

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import List from './containers/List';
+import Header from './components/Header/Header';
+import MenuHeader from './components/Header/Menu';
+import ListRight from './containers/ListRight';
+import '../src/css/index.css'
+import Footer from './components/Footer/Footer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  return (
+    <div className='container-fluid bg-app '>
+      <nav className='navbar sticky-top navbar-light bg-dark'>
+        <Header/>
+      </nav>
+      <div className="menu"><MenuHeader/></div>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-9">
+          <List />
+          </div>
+          <div className="col-sm-3">
+          <ListRight/>
+          </div>
+        </div>
+      </div>
+      <footer>
+        
+        <Footer></Footer>
+      </footer>
+    </div>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'));
